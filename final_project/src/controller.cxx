8f06b32de90a8b012d757829436496bb91c681e6
mydgm <101431153+mydgm@users.noreply.github.com>
2022-03-12 16:55:08 -0600

    Update controller.cxx

--- final_project/src/controller.cxx
+++ final_project/src/controller.cxx
@@ -16,9 +16,47 @@ Controller::Controller(int width, int height)
 void
 Controller::draw(ge211::Sprite_set& set)
 {
-view_.draw(set);
+    view_.draw(set);
+}
+double max_frame = 20;
+double update_frame = 0;
+
+
+
+void Controller::on_frame(double)
+{
+if(!model_.is_game_over()) {
+    Piece p = model_.active_piece_;
+    if (update_frame >= max_frame) {
+        model_.move_down();
+        update_frame = 0;
+
+    }
+    update_frame += 1;
 }
 
+}
+void Controller::on_key(ge211::Key key)
+{
+
+    if (!model_.is_game_over()) {
+
+        if (key == key.left()) {
+            model_.move_left(model_.active_piece());
+
+        }
+        if (key == key.right()) {
+            model_.move_right(model_.active_piece());
+        }
+        if (key == key.down()) {
+            model_.move_down();
+        }
+        if (key == key.up()) {
+           model_.rotate_piece();
+
+        }
+    }
+}
 View::Dimensions Controller::initial_window_dimensions() const
 {
     return view_.initial_window_dimensions();
@@ -28,3 +66,4 @@ std::string Controller::initial_window_title() const
 {
     return view_.initial_window_title();
 }
+
