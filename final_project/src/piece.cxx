d14a0dba7e8ef50123c515e01caff2f9509fca1a
youssefibrahim36 <101436840+youssefibrahim36@users.noreply.github.com>
2022-03-12 16:43:13 -0600

    Update piece.cxx

--- final_project/src/piece.cxx
+++ final_project/src/piece.cxx
@@ -3,15 +3,16 @@
 #include <iostream>
 #include <vector>
 
+static const double initial_velocity = 8;
+
+ge211::geometry::Posn<int> top_left_=  {4,0};
 
 Piece::Piece(Piece_type type, std::vector<Piece::Position>& pos,
              std::vector<Piece::Position>& actual_pos)
-        :
-        type_(type),
-        pos_(pos),
+        :velocity(initial_velocity),
+         pos_(pos),
+         type_(type),
         actual_pos_(actual_pos)
-
-
 {}
 
 ge211::geometry::Posn<int>
@@ -19,19 +20,6 @@ Piece::get_top_left(Piece p)
 {
     return top_left_;
 }
-
-ge211::geometry::Posn<int>
-Piece::move_top_left(Piece::Position move)
-{
-
-    ge211::geometry::Posn<int> new_tl = ge211::geometry::Posn<int>(0, 0);
-    new_tl.x = top_left_.x + move.x;
-    new_tl.y = top_left_.y + move.y;
-    return (new_tl);
-}
-
-
-
 
 
 std::vector<ge211::geometry::Posn<int>>
@@ -65,14 +53,13 @@ Piece create_piece(Piece_type type)
                  {1, 0},
                  {0, 1},
                  {1, 1}};
-        std::vector<ge211::Posn<int>>
-        actual_pos ={
-                {4,0},
-                 {5, 0},
-                 {4, 1},
-                 {5, 1}};
 
-        return Piece(type, pos,actual_pos);
+        std::vector<ge211::Posn<int>> actual_pos;
+
+        actual_pos.push_back({top_left_});
+        actual_pos.push_back({top_left_.x +1 , 0});
+        actual_pos.push_back({top_left_.x, top_left_.y + 1});
+        actual_pos.push_back({top_left_.x + 1, top_left_.y +1});
 
     }
     else if (type == Piece_type::line) {
@@ -82,28 +69,30 @@ Piece create_piece(Piece_type type)
                  {1, 1},
                  {2, 1},
                  {3, 1}};
-        std::vector<Piece::Position>
-                actual_pos =
-                {{4, 0},
-                 {5, 0},
-                 {6, 0},
-                 {7, 0}};
+        std::vector<Piece::Position> actual_pos;
+
+        actual_pos.push_back({top_left_});
+        actual_pos.push_back({top_left_.x + 1, top_left_.y});
+        actual_pos.push_back({top_left_.x + 2, top_left_.y});
+        actual_pos.push_back({top_left_.x + 3, top_left_.y});
         return Piece(type, pos,actual_pos);
 
     }
+
     else if (type == Piece_type::t_flipped) {
         std::vector<Piece::Position>
                 pos =
-                {{1,0},
-                 {0, 1},
-                 {1, 1},
-                 {2, 1}};
-        std::vector<Piece::Position>
-        actual_pos =
-                {{5,0},
-                 {4, 1},
-                 {5, 1},
-                 {6, 1}};
+                {{1,1},
+                 {0, 2},
+                 {1, 2},
+                 {2, 2}};
+
+        std::vector<Piece::Position> actual_pos;
+
+        actual_pos.push_back({top_left_});
+        actual_pos.push_back({top_left_.x + 1, top_left_.y + 1});
+        actual_pos.push_back({top_left_.x , top_left_.y + 2});
+        actual_pos.push_back({top_left_.x + 2, top_left_.y + 2});
 
         return Piece(type, pos,actual_pos);
 
@@ -116,12 +105,11 @@ Piece create_piece(Piece_type type)
                  {1, 0},
                  {1, 1},
                  {2, 1}};
-        std::vector<Piece::Position>
-                actual_pos =
-                {{4, 0},
-                 {5, 0},
-                 {5, 1},
-                 {6, 1}};
+        std::vector<Piece::Position> actual_pos;
+        actual_pos.push_back({top_left_});
+        actual_pos.push_back({top_left_.x + 1, top_left_.y });
+        actual_pos.push_back({top_left_.x + 1 , top_left_.y + 1});
+        actual_pos.push_back({top_left_.x + 2, top_left_.y + 1});
 
         return Piece(type, pos,actual_pos);
 
@@ -133,14 +121,15 @@ Piece create_piece(Piece_type type)
                  {0, 1},
                  {1, 1},
                  {2, 1}};
-        std::vector<Piece::Position>
-                actual_pos =
-                {{6, 0},
-                 {4, 1},
-                 {5, 1},
-                 {6, 1}};
+        std::vector<Piece::Position> actual_pos ;
+        actual_pos.push_back({top_left_});
+        actual_pos.push_back({top_left_.x + 1, top_left_.y});
+        actual_pos.push_back({top_left_.x + 2, top_left_.y});
+        actual_pos.push_back({top_left_.x + 2, top_left_.y + 1});
+
         return Piece(type, pos,actual_pos);
     }
+
     std::vector<Piece::Position>
             pos =
             {{0, 0},
@@ -153,6 +142,7 @@ Piece create_piece(Piece_type type)
              {0, 0},
              {0, 0},
              {0, 0}};
+
     return Piece(type, pos,actual_pos);
 }
 
@@ -177,41 +167,6 @@ Piece_type random_piece()
     }
     else {
         return Piece_type::t_flipped;
-    }
-}
-void
-Piece::rotate_piece()
-{
-
-    if (get_name() == Piece_type::line) {
-
-        /*for (int i = 0; i < 4; i++) {
-            Piece::Position diff = ge211::geometry::Posn<int>(copy_p.get_body()
-                                                              [i].x - get_body()[i].x, copy_p.get_body()[i].y - p
-                    .get_body()[i].y);
-            copy_p.pos_[i] = {p.pos_[i].y, p.pos_[i].x};
-
-            copy_p.actual_pos_[i] = {p.get_actual_body()[i].x + diff.x,
-                                           p.get_actual_body()[i].y + diff.y};
-
-        }*/
-    } else {
-
-        for (int i = 0; i < 4; i++) {
-            int intial_x = pos_[i].x;
-            int intial_y = pos_[i].y;
-            int real_intial_x = actual_pos_[i].x;
-            int real_intial_y = actual_pos_[i].y;
-            pos_[i] = {2 - intial_y, intial_x};
-            Piece::Position diff = ge211::geometry::Posn<int>(pos_[i].x -
-                                                              intial_x,
-                                                              pos_[i].y -
-                                                              intial_y);
-            actual_pos_[i] = {actual_pos_[i].x + diff.x, actual_pos_[i].y +
-                                                         diff.y};
-
-        }
-
     }
 }
 
