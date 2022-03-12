7de8312da37e055cdf9f931e18fa86132e545dec
mydgm <101431153+mydgm@users.noreply.github.com>
2022-03-12 16:31:11 -0600

    Update piece.cxx

--- final_project/src/piece.cxx
+++ final_project/src/piece.cxx
@@ -3,18 +3,37 @@
 #include <iostream>
 #include <vector>
 
-Piece::Piece(Piece_type type, std::vector<Piece::Position>& pos)
+
+Piece::Piece(Piece_type type, std::vector<Piece::Position>& pos,
+             std::vector<Piece::Position>& actual_pos)
         :
         type_(type),
-        pos_(pos)
+        pos_(pos),
+        actual_pos_(actual_pos)
+
 
 {}
 
 ge211::geometry::Posn<int>
 Piece::get_top_left(Piece p)
 {
-   return top_left_;
+    return top_left_;
 }
+
+ge211::geometry::Posn<int>
+Piece::move_top_left(Piece::Position move)
+{
+
+    ge211::geometry::Posn<int> new_tl = ge211::geometry::Posn<int>(0, 0);
+    new_tl.x = top_left_.x + move.x;
+    new_tl.y = top_left_.y + move.y;
+    return (new_tl);
+}
+
+
+
+
+
 std::vector<ge211::geometry::Posn<int>>
 Piece::get_body() const
 {
@@ -26,6 +45,13 @@ Piece::get_name() const
 {
     return type_;
 }
+
+std::vector<Piece::Position>
+Piece::get_actual_body() const
+{
+    return actual_pos_;
+}
+
 
 
 Piece create_piece(Piece_type type)
@@ -39,8 +65,14 @@ Piece create_piece(Piece_type type)
                  {1, 0},
                  {0, 1},
                  {1, 1}};
+        std::vector<ge211::Posn<int>>
+        actual_pos ={
+                {4,0},
+                 {5, 0},
+                 {4, 1},
+                 {5, 1}};
 
-        return Piece(type, pos);
+        return Piece(type, pos,actual_pos);
 
     }
     else if (type == Piece_type::line) {
@@ -50,18 +82,30 @@ Piece create_piece(Piece_type type)
                  {1, 1},
                  {2, 1},
                  {3, 1}};
-        return Piece(type, pos);
+        std::vector<Piece::Position>
+                actual_pos =
+                {{4, 0},
+                 {5, 0},
+                 {6, 0},
+                 {7, 0}};
+        return Piece(type, pos,actual_pos);
 
     }
     else if (type == Piece_type::t_flipped) {
         std::vector<Piece::Position>
                 pos =
-                {{1,1},
-                 {0, 2},
-                 {1, 2},
-                 {1, 2}};
+                {{1,0},
+                 {0, 1},
+                 {1, 1},
+                 {2, 1}};
+        std::vector<Piece::Position>
+        actual_pos =
+                {{5,0},
+                 {4, 1},
+                 {5, 1},
+                 {6, 1}};
 
-        return Piece(type, pos);
+        return Piece(type, pos,actual_pos);
 
     }
     else if (type == Piece_type::z) {
@@ -72,8 +116,14 @@ Piece create_piece(Piece_type type)
                  {1, 0},
                  {1, 1},
                  {2, 1}};
+        std::vector<Piece::Position>
+                actual_pos =
+                {{4, 0},
+                 {5, 0},
+                 {5, 1},
+                 {6, 1}};
 
-        return Piece(type, pos);
+        return Piece(type, pos,actual_pos);
 
     }
     else if (type == Piece_type::L_shape) {
@@ -83,7 +133,13 @@ Piece create_piece(Piece_type type)
                  {0, 1},
                  {1, 1},
                  {2, 1}};
-        return Piece(type, pos);
+        std::vector<Piece::Position>
+                actual_pos =
+                {{6, 0},
+                 {4, 1},
+                 {5, 1},
+                 {6, 1}};
+        return Piece(type, pos,actual_pos);
     }
     std::vector<Piece::Position>
             pos =
@@ -91,7 +147,13 @@ Piece create_piece(Piece_type type)
              {0, 0},
              {0, 0},
              {0, 0}};
-    return Piece(type, pos);
+    std::vector<Piece::Position>
+            actual_pos =
+            {{0, 0},
+             {0, 0},
+             {0, 0},
+             {0, 0}};
+    return Piece(type, pos,actual_pos);
 }
 
 
@@ -117,41 +179,39 @@ Piece_type random_piece()
         return Piece_type::t_flipped;
     }
 }
-
-
-Piece rotate_piece(Piece p)
+void
+Piece::rotate_piece()
 {
-    Piece copy_p = p;
-    if (p.get_name() == Piece_type::line){
-        int i = 0;
-        for (auto pos : p.get_body() ) {
-            copy_p.get_body()[i] = {pos.y, pos.x};
+
+    if (get_name() == Piece_type::line) {
+
+        /*for (int i = 0; i < 4; i++) {
             Piece::Position diff = ge211::geometry::Posn<int>(copy_p.get_body()
-                    [i].x - p.get_body()[i].x, copy_p.get_body()[i].y - p
+                                                              [i].x - get_body()[i].x, copy_p.get_body()[i].y - p
                     .get_body()[i].y);
-            copy_p.get_actual_body()[i] = {p.get_actual_body()[i].x + diff.x,
+            copy_p.pos_[i] = {p.pos_[i].y, p.pos_[i].x};
+
+            copy_p.actual_pos_[i] = {p.get_actual_body()[i].x + diff.x,
                                            p.get_actual_body()[i].y + diff.y};
 
+        }*/
+    } else {
 
-
-            i++;
+        for (int i = 0; i < 4; i++) {
+            int intial_x = pos_[i].x;
+            int intial_y = pos_[i].y;
+            int real_intial_x = actual_pos_[i].x;
+            int real_intial_y = actual_pos_[i].y;
+            pos_[i] = {2 - intial_y, intial_x};
+            Piece::Position diff = ge211::geometry::Posn<int>(pos_[i].x -
+                                                              intial_x,
+                                                              pos_[i].y -
+                                                              intial_y);
+            actual_pos_[i] = {actual_pos_[i].x + diff.x, actual_pos_[i].y +
+                                                         diff.y};
 
         }
+
     }
-    else {
-        int i  = 0;
-        for (auto pos : p.get_body() ){
-            copy_p.get_body()[i] = {2-pos.y, pos.x};
-            Piece::Position diff = ge211::geometry::Posn<int>(copy_p.get_body()
-                                                              [i].x - p.get_body()[i].x, copy_p.get_body()[i].y - p
-                    .get_body()[i].y);
-            copy_p.get_actual_body()[i] = {p.get_actual_body()[i].x + diff.x,
-                                           p.get_actual_body()[i].y + diff.y};
-            i++ ;
-
-        }
-    }
-
-
-    return copy_p;
 }
+
